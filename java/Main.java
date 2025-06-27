0b5538ebc55d2fbc3cd25105047a669366d14c90
S. Eshwar Durga Prasad <durgaprasadeshwar@gmail.com>
2025-06-27 16:41:58 +0530

    Add files via upload

---
+++ java/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        a[i]=s.nextInt();
+        
+        int k=s.nextInt();
+        
+        for(int i=n-k;i<n;i++)
+        System.out.print(a[i]+" ");
+        
+        for(int i=0;i<n-k;i++)
+        System.out.print(a[i]+" ");
+	}
+}
